56a9ea268dbf4dc87c5378cf16d273897621edcd
fallingleaf1114 <m15392424198@gmail.com>
2023-11-13 22:28:19 +0100

    0.1

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -5,7 +5,7 @@ from math_quiz import function_A, function_B, function_C
 class TestMathGame(unittest.TestCase):
 
     def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+        """Test if random numbers generated are within the specified range."""
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
@@ -13,18 +13,29 @@ class TestMathGame(unittest.TestCase):
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        """Test if the result is one of the specified operators."""
+        operators = ['+', '-', '*']
+        for _ in range(1000):  # Test a large number of random values
+            operator = function_B()
+            self.assertIn(operator, operators)
 
     def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        """Test the correctness of function_C for various scenarios."""
+        test_cases = [
+            (5, 2, '+', '5+2', 7),
+            (3, 4, '*', '3*4', 12),
+            (10, 7, '-', '10-7', 3),
+            # Add more test cases
+            (1, 5, '*', '1*5', 5),
+            (8, 2, '+', '8+2', 10),
+            (6, 3, '-', '6-3', 3),
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for numb1, numb2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = function_C(numb1, numb2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="dsss_homework_2",
+    version="0.1",
+    author="fallingleaf1114",
+    author_email="m15392424198@gmail.com",
+    packages=find_packages(),
+)
